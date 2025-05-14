f25f269577b3e33791e627e3e02c4d46a2960d68
Gordon Watts <gwatts@uw.edu>
2025-05-14 15:55:34 +0200

    Remove bogus test code

--- calratio_training_data/training_query.py
+++ calratio_training_data/training_query.py
@@ -380,44 +380,6 @@ def convert_to_training_data(data: Dict[str, ak.Array]) -> ak.Record:
         with_name="Momentum3D",
     )
 
-    # clusters = ak.zip(
-    #     {
-    #         "eta": data.clus_eta,  # type: ignore
-    #         "phi": data.clus_phi,  # type: ignore
-    #         "pt": data.clus_pt,  # type: ignore
-    #         "l1hcal": data.clus_l1hcal,  # type: ignore
-    #         "l2hcal": data.clus_l2hcal,  # type: ignore
-    #         "l3hcal": data.clus_l3hcal,  # type: ignore
-    #         "l4hcal": data.clus_l4hcal,  # type: ignore
-    #         "l1ecal": data.clus_l1ecal,  # type: ignore
-    #         "l2ecal": data.clus_l2ecal,  # type: ignore
-    #         "l3ecal": data.clus_l3ecal,  # type: ignore
-    #         "l4ecal": data.clus_l4ecal,  # type: ignore
-    #         "time": data.clus_time,  # type: ignore
-    #     },
-    #     with_name="Momentum3D",
-    # )
-    # tracks = ak.zip(
-    #     {
-    #         "pt": nearby_tracks.pt,
-    #         "eta": nearby_tracks.eta,
-    #         "phi": nearby_tracks.phi,
-    #     },
-    #     with_name="Momentum3D",
-    # )
-
-    # for i in range(len(clusters)):
-    #     if len(clusters[i]) != len(tracks[i]) or (len(clusters[i]) != len(jets[i])):
-    #         print(clusters[i].type.show())
-    #         print(tracks[i].type.show())
-    #         print(jets[i].type.show())
-    #         print(i)
-
-    # print(clusters[25].type.show())
-
-    # print(training_data.type.show())
-    # print(training_data[0].type.show())
-
     return training_data
 
 
