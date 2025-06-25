5e36085461942e004236543fc24620619b6f0eb8
Gordon Watts <gwatts@uw.edu>
2025-06-24 22:28:10 -0700

    Mask out LLP's we want

--- calratio_training_data/constants.py
+++ calratio_training_data/constants.py
@@ -6,3 +6,17 @@ JET_MSEG_DELTA_PHI = 0.2
 
 # The delta R between a LLP and a jet for the jet to be considered from the LLP
 LLP_JET_DELTA_R = 0.4
+
+# Info specifying what range LLPs are valid for training in.
+# These are *detector* coordinates, not relative to the PV's location.
+
+# Below this LLP's are central. Above they are in the endcap.
+LLP_central_eta_cut = 1.4
+
+# Calorimeter Lxy is 1200 mm to 4000 mm
+LLP_Lxy_min = 1200
+LLP_Lxy_max = 4000
+
+# Calorimeter Lz is 3500 mm to 6000 mm (abs)
+LLP_Lz_min = 3500
+LLP_Lz_max = 6000
