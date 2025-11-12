8a39b5e406104f476e3e6c5dab643ed599d75477
algolub <agolub9@uw.edu>
2025-11-12 17:02:31 -0600

    Basic CLI support for combining datasets

--- calratio_training_data/combining.py
+++ calratio_training_data/combining.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional, List
 from pathlib import Path
+import awkward as ak
 
 
 # Data class for combine configuration options
@@ -10,5 +11,8 @@ class CombineConfig:
     event_filter: Optional[str] = None
 
 
-def combine_training_data(input_file: List[Path], config: CombineConfig):
-    print("hello!")
+def combine_training_data(input_files: List[str], config: CombineConfig):
+    arrays = [ak.from_parquet(f) for f in input_files]
+    combined = ak.concatenate(arrays, axis=0)
+    ak.to_parquet(combined, config.output_path)
+    return combined
