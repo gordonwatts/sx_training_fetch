792e739b22364b566b57233ebc923d2afb931132
algolub <agolub9@uw.edu>
2025-12-03 18:55:04 -0600

    Added ability for eventfilter input using regex

--- calratio_training_data/combining.py
+++ calratio_training_data/combining.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import Optional, List
 from pathlib import Path
 import awkward as ak
+import re
 
 
 # Data class for combine configuration options
@@ -12,7 +13,23 @@ class CombineConfig:
 
 
 def combine_training_data(input_files: List[Path], config: CombineConfig):
-    arrays = [ak.from_parquet(f) for f in input_files]
+
+    # Creating a filter for the events
+    if config.event_filter is not None:
+        # Apply the filter to the events
+        arrays = []
+
+        # using regex to get the mask
+        m = re.search(r"% (\d+)\s*==\s*(\d+)", config.event_filter)
+
+        # looping through the input files and applying the filter to them
+        for f in input_files:
+            arr = ak.from_parquet(f)
+            mask = arr["eventNumber"] % int(m.group(1)) == int(m.group(2))
+            filtered = arr[mask]
+            arrays.append(filtered)
+    else:
+        arrays = [ak.from_parquet(f) for f in input_files]
     combined = ak.concatenate(arrays, axis=0)
     ak.to_parquet(combined, config.output_path)
     return combined
